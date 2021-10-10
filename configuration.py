e857ee9a30aaeff53801f7d3956ab13c56a62e67
your_crowley <your_crowley@your-crowleys-MacBook-Pro.local>
2021-10-10 17:08:11 +0300

    init2

--- configuration.py
+++
@@ -1,1 +0,0 @@
-SERVICE_URL = 'https://gorest.co.in/public/v1/users'
--- src/enums/global_enums.py
+++
@@ -1,5 +0,0 @@
-from enum import Enum
-
-
-class GlobalErrors(Enum):
-    WRONG_STATUS_CODE = 'Status code is different than expected'
