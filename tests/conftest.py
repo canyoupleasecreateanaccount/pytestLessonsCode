4c801ac23f08bd746925fe715aa6a639cd701f98
your_crowley <your_crowley@your-crowleys-MacBook-Pro.local>
2021-10-23 17:49:23 +0300

    lesson about marks

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,10 @@ def get_number():
 
 
 def _calculate(a, b):
-    return a + b
+    if isinstance(a, int) and isinstance(b, int):
+        return a + b
+    else:
+        return None
 
 
 @pytest.fixture
@@ -23,3 +26,4 @@ def make_number():
     number = randrange(1, 1000, 5)
     yield number
     print(f"Number at home {number}")
+
--- tests/users/test_users.py
+++ tests/users/test_users.py
@@ -1,12 +1,28 @@
+import pytest
+
 from src.baseclasses.response import Response
 from src.schemas.user import User
 
 
 def test_getting_users_list(get_users, make_number):
     Response(get_users).assert_status_code(200).validate(User)
-    print(make_number)
+    # print(make_number)
 
 
+@pytest.mark.development
+@pytest.mark.production
+@pytest.mark.skip('[ISSUE-23414] Issue with network connection')
 def test_another():
     assert 1 == 1
 
+
+@pytest.mark.development
+@pytest.mark.parametrize('first_value, second_value, result', [
+    (1, 2, 3),
+    (-1, -2, -3),
+    (-1, 2, 1),
+    ('b', -2, None),
+    ('b', 'b', None)
+])
+def test_calculator(first_value, second_value, result, calculate):
+    assert calculate(first_value, second_value) == result
